2021ec48a3d09103fec6bebe79bd232312d787a3
likvidator <max-kor4@yandex.ru>
2017-06-14 00:06:49 +0300

    Test gitignore

---
+++ Controller/connection.php
@@ -0,0 +1,19 @@
+<?php
+
+function get_connect(){
+  $host = 'localhost';
+  $database = 'home';
+  $user = 'root';
+
+
+  $password = '';
+
+  global $connect;
+  $connect = mysqli_connect($host, $user, $password, $database)
+      or die("Ошибка при подключении к базе" . mysqli_error($link));
+  mysqli_set_charset($connect,'utf8');
+
+  return $connect;
+}
+
+?>
